fa326eab45f6d32b4cc7bddaabece80078757e29
bingling_sama <65523047+bingling-sama@users.noreply.github.com>
2021-07-14 22:07:59 +0800

    配置文件模板
    用于pr时本地测试

---
+++ src/main/java/cn/booling/bakahdt/Secret.java
@@ -0,0 +1,9 @@
+package cn.booling.bakahdt;
+
+import net.mamoe.mirai.contact.Group;
+
+public class Secret {
+    public static final long BOT_ID = 12345678910L;
+    public static final String BOT_PWD = "12345678";
+    public static final String BOT_NAME = "BOT";
+}
